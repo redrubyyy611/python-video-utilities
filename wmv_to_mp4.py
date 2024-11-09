8766adf0549cd7963da466af20dc2a150364e3bb
Qi'ra <qirarodriguez@gmail.com>
2024-11-09 18:35:52 -0500

    remove commented code

--- wmv_to_mp4.py
+++ wmv_to_mp4.py
@@ -11,8 +11,3 @@ should_keep_audio = len(sys.argv) == 3 and sys.argv[1] in ["-na", "--no-audio"]
 
 clip = mpy.VideoFileClip(filename=input_file)
 clip.write_videofile(output_file, audio=should_keep_audio)
-
-# clip1 = mpy.VideoFileClip("mp4/scammer-pt1.mp4")
-# clip2 = mpy.VideoFileClip("mp4/scammer-pt2.2.mp4")
-# final = mpy.concatenate_videoclips([clip1, clip2])
-# final.write_videofile("mp4/scammer.mp4", audio=False)
